b0624126b9631b3dcf92853d9b59d53b5fa7b334
Derrick Reimer <derrickreimer@gmail.com>
2020-07-17 00:18:49 -0500

    Spike out the strategy

--- lib/ueberauth/strategy/zoom/oauth.ex
+++ lib/ueberauth/strategy/zoom/oauth.ex
@@ -1,5 +1,75 @@
 defmodule Ueberauth.Strategy.Zoom.OAuth do
   @moduledoc """
-  OAuth2 for Zoom.
+  An implementation of OAuth2 for Zoom.
+
+  To add your `client_id` and `client_secret` include these values in your configuration.
+
+      config :ueberauth, Ueberauth.Strategy.Zoom.OAuth,
+        client_id: System.get_env("ZOOM_CLIENT_ID"),
+        client_secret: System.get_env("ZOOM_CLIENT_SECRET")
   """
+  use OAuth2.Strategy
+
+  @defaults [
+    strategy: __MODULE__,
+    site: "https://api.zoom.us",
+    authorize_url: "https://zoom.us/oauth/authorize",
+    token_url: "https://zoom.us/oauth/token"
+  ]
+
+  # Public API
+
+  @doc """
+  Construct a client for requests to Zoom.
+
+  This will be setup automatically for you in `Ueberauth.Strategy.Zoom`.
+
+  These options are only useful for usage outside the normal callback phase of Ueberauth.
+  """
+  def client(opts \\ []) do
+    config = Application.get_env(:ueberauth, __MODULE__, [])
+    opts = @defaults |> Keyword.merge(opts) |> Keyword.merge(config)
+    json_library = Ueberauth.json_library()
+
+    opts
+    |> OAuth2.Client.new()
+    |> OAuth2.Client.put_serializer("application/json", json_library)
+  end
+
+  @doc """
+  Provides the authorize url for the request phase of Ueberauth. No need to call this usually.
+  """
+  def authorize_url!(params \\ [], opts \\ []) do
+    opts
+    |> client
+    |> OAuth2.Client.authorize_url!(params)
+  end
+
+  @doc """
+  Fetches a token.
+  """
+  def get_token(params \\ [], opts \\ []) do
+    OAuth2.Client.get_token(client(opts), params)
+  end
+
+  @doc """
+  Makes a GET request to the given URL.
+  """
+  def get(token, url, headers \\ [], opts \\ []) do
+    [token: token]
+    |> client()
+    |> OAuth2.Client.get(url, headers, opts)
+  end
+
+  # Strategy Callbacks
+
+  def authorize_url(client, params) do
+    OAuth2.Strategy.AuthCode.authorize_url(client, params)
+  end
+
+  def get_token(client, params, headers) do
+    client
+    |> put_header("accept", "application/json")
+    |> OAuth2.Strategy.AuthCode.get_token(params, headers)
+  end
 end
